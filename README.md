dc3f44873f3c8f846dd8e76094754acea8a8aacf
davidhirtz <david.hirtz@me.com>
2020-09-24 14:30:24 +0200

    Tinify

--- README.md
+++ README.md
@@ -5,9 +5,23 @@ This extension enables the Tinify API, used for [TinyPNG](https://tinypng.com) a
 
 Read more at [http://tinify.com](http://tinify.com).
 
-
 ### Installation
 
 ```
 composer require davidhirtz/yii2-media-tinify
+```
+
+### Setup
+
+Add your TinyPNG API key to `config/params.php` or use command line script:
+
+```
+php yii params/create tinifyApiKey "YOUR-TINYPNG-KEY"
+```
+
+```php
+<?php
+return [
+    'tinifyApiKey' => 'YOUR-TINYPNG-KEY',
+];
 ```
--- composer/Bootstrap.php
+++ composer/Bootstrap.php
@@ -7,9 +7,9 @@ use davidhirtz\yii2\media\s3\Module;
 use davidhirtz\yii2\skeleton\composer\BootstrapTrait;
 use yii\base\Application;
 use yii\base\BootstrapInterface;
-use Yii;
+use yii\base\Event;
 use yii\base\ModelEvent;
-use yii\debug\models\search\Event;
+use Yii;
 
 /**
  * Class Bootstrap
@@ -20,12 +20,33 @@ class Bootstrap implements BootstrapInterface
     use BootstrapTrait;
 
     /**
+     * Overrides the default implementation of {@link Transformation::createTransformation()} by using the Tinify API to
+     * manipulate the image. Tinify doesn't work on WEBP files.
      * @param Application $app
      */
     public function bootstrap($app)
     {
-        Event::on(Transformation::EVENT_AFTER_FIND, function (ModelEvent $event) {
-            Yii::debug($event->sender);
-        });
+        if (!empty($app->params['tinifyApiKey'])) {
+            Event::on(Transformation::class, Transformation::EVENT_BEFORE_TRANSFORMATION, function (ModelEvent $event) {
+                /** @var Transformation $transformation */
+                $transformation = $event->sender;
+
+                if (!$transformation->isWebp()) {
+                    \Tinify\setKey(Yii::$app->params['tinifyApiKey']);
+
+                    $image = \Tinify\fromFile($transformation->file->folder->getUploadPath() . $transformation->file->getFilename())->resize(array_filter([
+                        'method' => $transformation->width && $transformation->height ? 'cover' : 'scale',
+                        'width' => $transformation->width,
+                        'height' => $transformation->height,
+                    ]));
+
+                    $transformation->size = $image->toFile($transformation->getFilePath());
+                    $transformation->width = $image->result()->width();
+                    $transformation->height = $image->result()->height();
+
+                    $event->isValid = false;
+                }
+            });
+        }
     }
 }
